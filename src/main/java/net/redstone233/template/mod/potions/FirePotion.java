129747ab44cebb4a7645d7bb88aa088d4f76548c
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-18 22:34:50 +0800

    Create FirePotion.java

---
+++ src/main/java/net/redstone233/template/mod/potions/FirePotion.java
@@ -0,0 +1,11 @@
+package net.redstone233.template.mod.potions;
+
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.potion.Potion;
+
+public class FirePotion extends Potion {
+
+    public FirePotion(StatusEffectInstance[] effect) {
+        super(effect);
+    }
+}
