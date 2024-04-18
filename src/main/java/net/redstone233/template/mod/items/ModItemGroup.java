c0d2ef014a1493728b31b84bf06df134107fc52d
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-18 22:26:18 +0800

    Add files via upload

---
+++ src/main/java/net/redstone233/template/mod/items/ModItemGroup.java
@@ -0,0 +1,35 @@
+package net.redstone233.template.mod.items;
+
+import net.redstone233.template.mod.TemplateMod;
+import net.redstone233.template.mod.block.ModBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroup {
+
+    @SuppressWarnings("unused")
+    private static final ItemGroup ITEM_GROUP = 
+        Registry.register(Registries.ITEM_GROUP, 
+        new Identifier(TemplateMod.MOD_ID, "trsm_group"), 
+            FabricItemGroup.builder()
+                .icon(() -> new ItemStack(ModItems.PROSPECTOR))
+                .displayName(Text.translatable("itemGroup.tlm_group"))
+                .entries((context, entries) -> {
+                    entries.add(ModBlocks.REFORGED_BLOCK);
+                    entries.add(ModBlocks.FIRE_REFORGED_BLOCK);
+                    entries.add(ModItems.PROSPECTOR);
+                    
+                })
+                .build());
+
+    public static void registerModItemGroup() {
+        
+    }
+}
+
+// C:\Users\Redstone\.gradle\wrapper\dists\gradle-8.6-bin\afr5mpiioh2wthjmwnkmdsd5w\gradle-8.6
---
+++ src/main/java/net/redstone233/template/mod/items/ModItems.java
@@ -0,0 +1,27 @@
+package net.redstone233.template.mod.items;
+
+import net.redstone233.template.mod.TemplateMod;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item PROSPECTOR = registerItems("prospector", 
+        new Prospectetor(new FabricItemSettings().maxDamage(1000)));
+
+    private static Item registerItems(String name,Item item) {
+        return Registry.register(Registries.ITEM,
+            new Identifier(TemplateMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIG);
+    }
+}
