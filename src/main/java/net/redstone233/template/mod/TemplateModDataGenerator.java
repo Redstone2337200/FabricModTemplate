4e077f724418ed0c85814e5892cf2a8ecbe041fd
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-04-18 20:53:51 +0800

    Add files via upload

---
+++ src/main/java/net/redstone233/template/mod/TemplateModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.redstone233.template.mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TemplateModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
